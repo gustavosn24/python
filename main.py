2cce7a2b533791e64d3795496d95a1eb9731cd14
gustavosn <jose.gustavo1@escolar.ifrn.edu.br>
2026-03-17 21:49:43 -0300

    opcao 2

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-livros = {}
+livros = {
+    'metamoforse': {
+        'titulo': 'metamoforse',
+        'autor': 'gu',
+        'anoPubli': 12,
+        'quantidade': 3
+    },
+    'o rei': {
+        'titulo': 'o rei',
+        'autor': 'vc',
+        'anoPubli': 23,
+        'quantidade': 8
+    },
+    'dom casmurro': {
+        'titulo': 'dom casmurro',
+        'autor': 'machado de assis',
+        'anoPubli': 1899,
+        'quantidade': 5
+    },
+    '1984': {
+        'titulo': '1984',
+        'autor': 'george orwell',
+        'anoPubli': 1949,
+        'quantidade': 7
+    },
+    'o pequeno principe': {
+        'titulo': 'o pequeno principe',
+        'autor': 'antoine de saint-exupery',
+        'anoPubli': 1943,
+        'quantidade': 10
+    }
+}
 
 #      cadrastro de livro      #
 def cadastrar_livro():
@@ -17,6 +48,10 @@ def cadastrar_livro():
         livros[titulo] = livro
         print("livro cadrastado com suscesso!")
 
+def listar_livro():
+    for i, (chave, valor) in enumerate(livros.items(), start=1):
+        print(f"{i} - {chave}")
+
 
 
 while True:
@@ -25,13 +60,19 @@ while True:
     print("--------------------")
     print(" 1 - Cadrastrar Livro")
     print(" 2 - Listar Livros")
+    print(" 3 - Emprestimo de Livros")
     print(" 0 - Sair")
     opcao = int(input("escolha uma opção: "))
 
     if opcao == 1:
         cadastrar_livro()
     
-    # elif opcao == 2:
+    elif opcao == 2:
+        print(" ")
+        listar_livro()
+        print(" ")
+    
+    # elif opcao == 3:
 
         
     else:
